189a95e5ff3f9f0a1466e9dcd5e6ac7c02d75c42
alexq <alexquezada10@gmail.com>
2025-11-15 13:55:05 -0500

    add donate button

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,14 @@ export default function RootLayout({
                   </Link>
                 </div>
               </div>
+              <div>
+                <Link
+                  href="https://donate.stripe.com/8x2bJ10CHak14YydGI9bO03"
+                  className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md text-sm font-medium transition-colors"
+                >
+                  Donate
+                </Link>
+              </div>
             </div>
           </div>
         </nav>
